383e96d5c534075152355258e3b26de780718ae0
zhongjunhaoz <2236241424@qq.com>
2019-06-04 17:09:31 +0800

    vue 路径 跳转

---
+++ VUE/VUE-Router/router/src/components/LookOne.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="look">
+        {{message}}
+        {{$router.params}}
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Look',
+    data () {
+        return {
+            message:'sks'
+        }
+    }
+}
+</script>
---
+++ VUE/VUE-Router/router/src/components/LookTwo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="look">
+        {{message}}
+        <p>接受的参数： {{$route.params.userId}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Look',
+    data () {
+        return {
+            message:'m416'
+        }
+    }
+}
+</script>
